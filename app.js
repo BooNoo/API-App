cf2a28be85706da5edc38f3ca8366202fb58beae
vladimirmikhaylov <boonoovova@gmail.com>
2018-05-06 19:49:39 +1000

    try heroku

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
 const morgan = require('morgan');
+const PORT = process.env.PORT || 3003;
 
 var userRouter = require('./routes/user');
 
 app.use(morgan('short'));
-app.use('/users', userRouter);
+// app.use('/users', userRouter);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('./public'));
 
-app.listen(3002, function () {
-    console.log("server up");
+app.get("/", function (req, res) {
+    res.send("hello world");
+});
+
+app.listen(PORT, function () {
+    console.log("server up on: " + PORT);
 });
