8f9eb5940b7387febed8b985620ad635e594fe46
vladimirmikhaylov <boonoovova@gmail.com>
2018-05-06 18:58:56 +1000

    server with basic router

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
-var router = express.Router();
 const morgan = require('morgan');
 
 var userRouter = require('./routes/user');
 
-app.use(morgan('combined'));
+app.use(morgan('short'));
 app.use('/users', userRouter);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('./public'));
 
 app.listen(3002, function () {
     console.log("server up");
 });
-
-router.get("/", function (req, res) {
-    console.log("response");
-    res.send("Hello world");
-});
-
-
-
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,15 @@
 const express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'werTT75&',
-    database: 'testmysql'
+    database: 'users_test'
 });
 
 connection.connect(function (err) {
@@ -23,8 +26,24 @@ router.get("/", function (req, res) {
 });
 
 router.get("/:id", function (req, res) {
-    connection.query('SELECT * FROM users WHERE id = ?',[req.params.id], function (err, rows, fields) {
+    connection.query('SELECT * FROM users WHERE id = ?', [req.params.id], function (err, rows, fields) {
         res.json(rows);
+    });
+});
+
+router.post("/create", function (req, res) {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+
+    const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)";
+    connection.query(queryString, [firstName, lastName], function (err, results, fields) {
+        if (err) {
+            console.log("Failed insert user" + err);
+            res.sendStatus(500);
+            return
+        }
+        console.log("insert a new user with id:", results.insertId);
+        res.end();
     });
 });
 
