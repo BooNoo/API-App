a59884f30264229731beebf8480a05808027633d
vladimirmikhaylov <boonoovova@gmail.com>
2018-05-18 12:38:16 +1000

    corns

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const PORT = process.env.PORT || 3003;
 const userRouter = require('./routes/user');
 
-const corsm = function(req, res, next) {
+const corsm = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 };
 
-app.use(morgan('short'));
+// app.use(morgan('short'));
 app.use('/api/users', corsm, userRouter);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
