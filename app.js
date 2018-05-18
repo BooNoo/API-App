2fbcbb61127344916f8dfebfd0869e6cc8990b0f
vladimirmikhaylov <boonoovova@gmail.com>
2018-05-18 11:33:20 +1000

    corns

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const app = express();
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3003;
 const userRouter = require('./routes/user');
-const cors = require('cors');
+
+const corsm = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+};
 
 app.use(morgan('short'));
-app.use('/api/users', userRouter);
+app.use('/api/users', corsm, userRouter);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('./public'));
-
-
-app.use(function (req, res, next) {
-});
 
 app.get("/", function (req, res) {
     res.send("hello world");
--- routes/user.js
+++ routes/user.js
@@ -2,36 +2,8 @@ const express = require('express');
 var router = express.Router();
 const bodyParser = require("body-parser");
 var db = require('../models/dbconnection');
-// var fs = require("fs");
-// var firebaseAPI = require("firebase");
-// const googleStorage = require('@google-cloud/storage');
-//
-// const Multer = require('multer');
-// //
-// var config = {
-//     apiKey: "AIzaSyC4vjCzqWwP-vfaijAyGaJ_fLAbgIql0jE",
-//     authDomain: "historymap-1994.firebaseapp.com",
-//     databaseURL: "https://historymap-1994.firebaseio.com",
-//     projectId: "historymap-1994",
-//     storageBucket: "historymap-1994.appspot.com",
-//     messagingSenderId: "982265422411"
-// };
-//
-// firebaseAPI.initializeApp(config);
-//
-// const storage = googleStorage({
-//     projectId: "historymap-199409",
-//     keyFilename: "./history-map-cloud.json"
-// });
-//
-// // var datastore = gcloud.datastore();
-// // var storage = gcloud.storage();
-// const bucket = storage.bucket('history-map.appspot.com');
-//
-//
-// function getPublicUrl (filename) {
-//     return 'https://storage.googleapis.com/${CLOUD_BUCKET}/${filename}';
-// }
+
+router.use(bodyParser.urlencoded({extended: false}));
 
 const corsm = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -40,9 +12,7 @@ const corsm = function(req, res, next) {
     next();
 };
 
-router.use(bodyParser.urlencoded({extended: false}));
-
-router.get("/", corsm, function (req, res) {
+router.get("/", function (req, res) {
     db.query('SELECT * FROM users', function (err, rows, fields) {
         res.json(rows);
     });
@@ -69,80 +39,5 @@ router.post("/create", function (req, res) {
         res.end();
     });
 });
-
-// router.post("/save-image", function (req, res) {
-//
-//     const email = "boonoovova@gmail.com";
-//     const password = "940920Djdf";
-//
-//     firebaseAPI.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // ...
-//     });
-//
-//     var storageRef = firebaseAPI.getDownloadURL()
-//
-//     //
-//     // res.sendFile(image);
-//
-//     // fs.readFile('./assets/image.png', function (err, content) {
-//     //     if (err) {
-//     //         res.writeHead(400, {'Content-type':'text/html'})
-//     //         console.log(err);
-//     //         res.end("No such image");
-//     //     } else {
-//     //         //specify the content type in the response will be an image
-//     //         res.writeHead(200,{'Content-type':'image/jpg'});
-//     //         res.end(content);
-//     //     }
-//     // });
-//
-//     // var image = fs.readFileSync("./assets/image.png");
-//     // var destination = 'uploads/12345/full.jpg';
-//     //
-//     //
-//     // const gcsname = Date.now() + image.name;
-//     // console.log(image);
-//     // const file = bucket.file(gcsname);
-//     //
-//     // const stream = file.createWriteStream({
-//     //     metadata: {
-//     //         contentType: image.mimetype
-//     //     }
-//     // });
-//     //
-//     // stream.on('error', function (err) {
-//     //     image.cloudStorageError = err;
-//     //     next(err);
-//     // });
-//     //
-//     // stream.on('finish', function () {
-//     //     image.cloudStorageObject = gcsname;
-//     //     file.makePublic().then(function () {
-//     //         image.cloudStoragePublicUrl = getPublicUrl(gcsname);
-//     //         next();
-//     //     });
-//     // });
-//     //
-//     // stream.end(image.buffer);
-//
-//     // res.end(image)
-//
-//     // const blob = bucket.file(image.originalname);
-//     // const blobStream = blob.createWriteStream();
-//     // //
-//     // bucket.upload("./assets/image.png", { destination: "images/1.jpg" }, function(err, file) {
-//     //     if (err) {
-//     //         console.log(err);
-//     //         res.end(err)
-//     //     } else {
-//     //         res.end(file)
-//     //     }
-//     // });
-//
-//
-// });
 
 module.exports = router;
